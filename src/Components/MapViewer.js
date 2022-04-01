ae019df32aa22fc29cee5d81c7209c0c8ac3ab6e
Mouzam <mouzam.ali@luminogics.com>
2022-04-01 19:01:06 +0500

    Fixed color of map

--- src/Components/MapViewer.js
+++ src/Components/MapViewer.js
@@ -1,72 +1,74 @@
-import React, { useRef, useEffect, useState } from 'react'
-import geojson2h3 from 'geojson2h3';
-import polygon from '../Utilities/coordinates';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { token } from '../Utilities/constant';
+import React, { useRef, useEffect, useState } from "react";
+import geojson2h3 from "geojson2h3";
+import polygon from "../Utilities/coordinates";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { token } from "../Utilities/constant";
+import { layer } from "@fortawesome/fontawesome-svg-core";
 mapboxgl.accessToken = token;
 
 function MapViewer({ leftCord, rightCord }) {
-  const mapContainer = useRef(null)
-  let map = null
-  const [lng, setLng] = useState(-122.186688)
-  const [lat, setLat] = useState(37.759638)
-  const [zoom, setZoom] = useState(9)
-  
+  const mapContainer = useRef(null);
+  let map = null;
+  const [lng, setLng] = useState(-122.186688);
+  const [lat, setLat] = useState(37.759638);
+  const [zoom, setZoom] = useState(9);
+
   useEffect(() => {
-    leftCord && setLng(leftCord)
-    rightCord && setLat(rightCord)
+    leftCord && setLng(leftCord);
+    rightCord && setLat(rightCord);
+
     map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/dark-v10',
+      style: "mapbox://styles/mapbox/dark-v10",
       center: [lng, lat],
-      zoom: zoom
-    })
+      zoom: zoom,
+    });
     map.addControl(
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
+        mapboxgl: mapboxgl,
       })
-    )
-    let done
-    let count = 0
+    );
+    let done;
+    let count = 0;
     do {
-      done = false
-      count += 1
+      done = false;
+      count += 1;
       try {
-        map.loaded()
-        done = true
-        initializeMap()
+        map.loaded();
+        done = true;
+        initializeMap();
       } catch (error) {
-        console.log('error')
+        console.log("error");
       }
-    } while (!done && count < 10)
-  }, [lng, lat])
+    } while (!done && count < 10);
+  }, [lng, lat]);
   let setHexagons = () => {
-    const layer = {}
+    const layer = {};
     polygon.features.forEach((feature) => {
-      const hexagons = geojson2h3.featureToH3Set(feature, 7)
+      const hexagons = geojson2h3.featureToH3Set(feature, 7);
       hexagons.forEach((h3Index) => {
-        layer[h3Index] = feature.properties.travelTime
-      })
-    })
-    return normalizeLayer(layer)
-  }
+        layer[h3Index] = feature.properties.travelTime;
+      });
+    });
+    return normalizeLayer(layer);
+  };
   function normalizeLayer(layer, zeroBaseline = false) {
-    const hexagons = Object.keys(layer)
+    const hexagons = Object.keys(layer);
     // Pass one, get max (and min if needed)
     const max = hexagons.reduce(
       (max, hex) => Math.max(max, layer[hex]),
       -Infinity
-    )
+    );
     const min = zeroBaseline
       ? 0
-      : hexagons.reduce((min, hex) => Math.min(min, layer[hex]), Infinity)
+      : hexagons.reduce((min, hex) => Math.min(min, layer[hex]), Infinity);
     // Pass two, normalize
     hexagons.forEach((hex) => {
-      layer[hex] = (layer[hex] - min) / (max - min)
-    })
-    return layer
+      layer[hex] = (layer[hex] - min) / (max - min);
+    });
+    return layer;
   }
   const initializeMap = async () => {
     let config = {
@@ -74,89 +76,112 @@ function MapViewer({ leftCord, rightCord }) {
       lat: 37.7923539,
       zoom: 10.5,
       fillOpacity: 0.75,
-      colorScale: ['#FF0000', '#008000', '#FFFF00', '#338c94']
-    }
-    const hexagons = geojson2h3.featureToH3Set(polygon, 8)
+      colorScale: ["#FF0000", "#008000", "#FFFF00", "#338c94"],
+    };
+    const hexagons = geojson2h3.featureToH3Set(polygon, 8);
     const feature = geojson2h3.h3SetToFeatureCollection(hexagons, (hex) => ({
-      value: Number(Math.floor(Math.random() * 100))
-    }))
-    console.log(feature)
-    map.on('load', () => {
-      const sourceId = 'h3-hexes'
-      const layerId = `${sourceId}-layer`
-      let source = map.getSource(sourceId)
+      value: Number(Math.floor(Math.random() * 100)),
+    }));
+    console.log(feature);
+    map.on("load", () => {
+      const sourceId = "h3-hexes";
+      const layerId = `${sourceId}-layer`;
+      let source = map.getSource(sourceId);
       // Add the source and layer if we haven't created them yet
       if (!source) {
         map.addSource(sourceId, {
-          type: 'geojson',
-          data: feature
-        })
+          type: "geojson",
+          data: feature,
+        });
         map.addLayer({
           id: layerId,
           source: sourceId,
-          type: 'fill',
+          type: "fill",
           interactive: false,
           paint: {
-            'fill-outline-color': 'rgba(0,0,0,0)'
-          }
-        })
-        source = map.getSource(sourceId)
+            "fill-outline-color": "rgba(0,0,0,0)",
+          },
+        });
+        source = map.getSource(sourceId);
       }
       // Add a new layer to visualize the polygon.
       map.addLayer({
-        id: 'outline',
-        type: 'fill',
+        id: "outline",
+        type: "fill",
         source: sourceId, // reference the data source
-        type: 'fill',
+        type: "fill",
         interactive: false,
         paint: {
-          'fill-outline-color': 'rgba(0,0,0,0)',
-          'fill-opacity': 0.5
-        }
-      })
+          "fill-outline-color": "rgba(0,0,0,0)",
+          "fill-opacity": 0.5,
+        },
+      });
       // Update the geojson data
-      source.setData(feature)
+      source.setData(feature);
       // Add a black outline around the polygon.
       map.addLayer({
         id: layerId,
-        type: 'line',
+        type: "line",
         source: sourceId,
         layout: {},
         paint: {
-          'line-color': '#000',
-          'line-width': 5
-        }
-      })
+          "line-color": "#000",
+          "line-width": 5,
+        },
+      });
       // map.setPaintProperty(layerId, 'fill-color', '#feb24c')
-      map.setPaintProperty(layerId, 'fill-color', {
-        property: 'value',
+      map.setPaintProperty(layerId, "fill-color", {
+        property: "value",
         stops: [
           [0, config.colorScale[0]],
           [33, config.colorScale[1]],
           [66, config.colorScale[2]],
-          [100, config.colorScale[3]]
-        ]
-      })
-    })
-  }
+          [100, config.colorScale[3]],
+        ],
+      });
+
+      // Create a popup, but don't add it to the map yet.
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      });
+
+      map.on("mouseenter", layerId, (e) => {
+      
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = "pointer";
+
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties;
+
+       
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+       // popup.setLngLat([-122.1835470199585, 37.80724101305343]).setHTML(description.value).addTo(map);
+      });
+    });
+  };
 
   return (
     <div
       style={{
-        width: '85%',
-        float: 'left'
+        width: "85%",
+        float: "left",
       }}
     >
-      <div className='sidebar'>
+      <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div
         ref={mapContainer}
         style={{
-          height: '600px'
+          height: "600px",
         }}
       />
     </div>
-  )
+  );
 }
-export default MapViewer
+export default MapViewer;
